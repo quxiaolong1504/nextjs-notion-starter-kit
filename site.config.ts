b8896d8550d5d46eacaea664ad1829ccc311deed
GreenPine <quxiaolong1504@gmail.com>
2022-11-24 14:15:53 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: '前端值得付费的工具',
-  domain: 'quxl.notion.site',
+  domain: 'https://quxl.notion.site/6be8acb6f1d8414b995290e6e406129d',
   author: '晓',
 
   // open graph metadata (optional)
